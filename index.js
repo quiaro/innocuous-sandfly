de98c60aad6323c5d2abef9fb3d52d7939f76ae6
David Quiros <david@quiros.online>
2017-09-22 09:44:41 -0600

    Set default value and attach datepicker to start date input

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ import './css/normalize.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css'
 import './css/styles.css';
 
+// Attach datepicker to start date input so it's easier for users
+// to input date values
+const now = new Date();
+
+$('#startDate').val(now.toLocaleDateString('en-US'))
+               .datepicker();
+
 $('#calendar').datepicker();
