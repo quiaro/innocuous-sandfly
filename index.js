eb5fac9a3eb83750ff9b91db4b693b42fcf4ad2f
David Quiros <david@quiros.online>
2017-09-22 11:06:56 -0600

    Homogenize date strings (calendar & holidays)

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import './css/normalize.css';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css'
 import './css/styles.css';
 
+function dateStringToDate(dateString) {
+  const [year, month, day] = dateString.split("-")
+  const date = new Date(year, month - 1, day);
+  return date.toLocaleDateString('en-US');
+}
+
 // Attach datepicker to start date input so it's easier for users
 // to input date values
 const now = new Date();
@@ -14,7 +20,10 @@ $('#startDate').val(now.toLocaleDateString('en-US'))
                .datepicker();
 
 $('#calendar').datepicker({
-  'daysOfWeekHighlighted': [0,6]
+  daysOfWeekHighlighted: [0,6],
+  beforeShowDay: (date) => {
+    console.log('Date is: ', date.toLocaleDateString('en-US'));
+  }
 }).hide();
 
 // Form submission
@@ -30,7 +39,7 @@ $('input[type="submit"]').on('click', (e) => {
 
   // Get list of holidays
   holidays.get().then(response => {
-    const holidayList = response.holidays.map(holiday => holiday.date);
+    const holidayList = response.holidays.map(holiday => dateStringToDate(holiday.date));
     console.log('holidays: ', holidayList);
   });
 
