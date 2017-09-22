7257ff6e82b2c641550e678d83223a6a948a5edf
David Quiros <david@quiros.online>
2017-09-22 09:26:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Running webpack and/or webpack dev server
+
+`npm run dev`
+Starts the webpack dev server at localhost:1357.
+
+`npm run build`
+Will run webpack to create a bundle which will be placed in `output.path` (webpack.config.js).
---
+++ index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery'
+import datepicker from 'bootstrap-datepicker'
+
+import './css/normalize.css';
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css'
+import './css/styles.css';
+
+$('#calendar').datepicker();
